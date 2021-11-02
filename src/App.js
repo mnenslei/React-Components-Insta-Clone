7a3c6661820785ba30bbbac034f2c17cf72518fd
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-02 19:26:54 -0400

    sheesh

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
 import './App.css';
+import dummyData from './dummy-data';
 
 const App = () => {
+  const [posts, setPosts] = useState(dummyData);
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
@@ -27,6 +31,9 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const updatedLikes = posts.map(post => {
+      return post.id === postId ? { ...post, likes: post.likes + 1} : post  
+    })
   };
 
   return (
